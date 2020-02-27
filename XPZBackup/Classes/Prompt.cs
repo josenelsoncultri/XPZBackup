a033dbefb803b6aa8ea832f68f77cc7ad649bef1
José Nelson <josefcultrifrc@gmail.com>
2020-02-27 12:56:19 -0300

    Criando classe para manipulação da execução dos processos

---
+++ XPZBackup/Classes/Prompt.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+
+namespace XPZBackup.Classes
+{
+    public class Prompt
+    {
+        public static void ExecutarBackup(Base b, string listaObjetos)
+        {
+
+        }
+
+        public static void DesligarMaquina()
+        {
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName = "shutdown.exe";
+            psi.Arguments = "-s -t 3600";
+            Process.Start(psi);
+        }
+    }
+}

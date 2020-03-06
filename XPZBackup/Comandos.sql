dcd040df7d0569e03fe79f7f883247a71ef07431
José Nelson <josefcultrifrc@gmail.com>
2020-03-06 13:07:00 -0300

    Alterando script SQL para recuperar corretamente a lista de objetos cujo backup será feito

--- XPZBackup/Comandos.sql
+++ XPZBackup/Comandos.sql
@@ -1,59 +1,44 @@
 WITH CTE_TIPOS_DADOS (TipoDado)
-	AS (
-		SELECT DISTINCT ET.EntityTypeName FROM <BANCO_DADOS>.dbo.EntityVersion EV
-		INNER JOIN <BANCO_DADOS>.dbo.EntityType ET ON (ET.EntityTypeId = EV.EntityTypeId)
-		INNER JOIN <BANCO_DADOS>.dbo.Entity E ON (E.EntityTypeId = EV.EntityTypeId AND E.EntityId = EV.EntityId AND E.EntityLastVersionId = EV.EntityVersionId)
-		WHERE SUBSTRING(EntityVersionName, 1, 3) NOT IN ('Audit')
-		AND EntityVersionTimestamp BETWEEN CONVERT(DATETIME, CONVERT(VARCHAR(10), GETDATE() - <QUANTIDADE_DIAS_BACKUP>, 103) + ' 00:00:00') AND CONVERT(DATETIME, (CONVERT(VARCHAR(10), GETDATE(), 103) + ' 23:59:59'))
-		AND ET.EntityTypeName NOT IN 
-			(
-				'Environments', 'DataStores', 'TranslationMessage', 'ThemeColor', 'ThemeColors', 'ColorPalette', 'Documentation', 'Help', 'MissingKBObject', 'WikiBlob',
-				'Events', 'Conditions', 'EXOStructure', 'FormDesigner', 'Layout', 'LocalizableImage', 'WinForm', 'Variables', 'SDTStructure', 'Structure',
-				'Rules', 'WebPanelDesigner', 'PatternInstance', 'PatternSettings', 'ThemeClass', 'SDTItemEntity', 'Module', 'SDTLevelEntity', 'WebForm', 'TableStructure',
-				'ModuleContent', 'IndexStructure', 'Root', 'TableIndexes', 'ThemeStyles', 'ThemeFonts', 'LNGStructure', 'DebugData'
-			)
-		AND ET.EntityTypeName NOT LIKE 'Udm.%'
-		AND EV.EntityVersionName NOT IN 
+AS (
+	SELECT DISTINCT ET.EntityTypeName FROM <BANCO_DADOS>.dbo.EntityVersion EV
+	INNER JOIN <BANCO_DADOS>.dbo.EntityType ET ON (ET.EntityTypeId = EV.EntityTypeId)
+	INNER JOIN <BANCO_DADOS>.dbo.Entity E ON (E.EntityTypeId = EV.EntityTypeId AND E.EntityId = EV.EntityId AND E.EntityLastVersionId = EV.EntityVersionId)
+	WHERE SUBSTRING(EntityVersionName, 1, 3) NOT IN ('Audit')
+	AND EntityVersionTimestamp BETWEEN CONVERT(DATETIME, CONVERT(VARCHAR(10), GETDATE() - <QUANTIDADE_DIAS_BACKUP>, 103) + ' 00:00:00') AND CONVERT(DATETIME, (CONVERT(VARCHAR(10), GETDATE(), 103) + ' 23:59:59'))
+	AND ET.EntityTypeName NOT IN 
+		(
+			'Environments', 'DataStores', 'TranslationMessage', 'ThemeColor', 'ThemeColors', 'ColorPalette', 'Documentation', 'Help', 'MissingKBObject', 'WikiBlob',
+			'Events', 'Conditions', 'EXOStructure', 'FormDesigner', 'Layout', 'LocalizableImage', 'WinForm', 'Variables', 'SDTStructure', 'Structure',
+			'Rules', 'WebPanelDesigner', 'PatternInstance', 'PatternSettings', 'ThemeClass', 'SDTItemEntity', 'Module', 'SDTLevelEntity', 'WebForm', 'TableStructure',
+			'ModuleContent', 'IndexStructure', 'Root', 'TableIndexes', 'ThemeStyles', 'ThemeFonts', 'LNGStructure', 'DebugData'
+		)
+	AND ET.EntityTypeName NOT LIKE 'Udm.%'
+	AND EV.EntityVersionName NOT IN 
+		(
+			'Procedure, Procedure'
+		)
+)
+SELECT STUFF(
+(
+	SELECT ';' + TipoDado + ':' +  
+	(
+		SELECT STUFF(
+		(
+			SELECT ',' + EV.EntityVersionName FROM <BANCO_DADOS>.dbo.EntityVersion EV
+			INNER JOIN <BANCO_DADOS>.dbo.EntityType ET ON (ET.EntityTypeId = EV.EntityTypeId)
+			INNER JOIN <BANCO_DADOS>.dbo.Entity E ON (E.EntityTypeId = EV.EntityTypeId AND E.EntityId = EV.EntityId AND E.EntityLastVersionId = EV.EntityVersionId)
+			WHERE SUBSTRING(EntityVersionName, 1, 3) NOT IN ('Audit')
+			AND EntityVersionTimestamp BETWEEN CONVERT(DATETIME, CONVERT(VARCHAR(10), GETDATE() - <QUANTIDADE_DIAS_BACKUP>, 103) + ' 00:00:00') AND CONVERT(DATETIME, (CONVERT(VARCHAR(10), GETDATE(), 103) + ' 23:59:59'))
+			AND ET.EntityTypeName = TipoDado
+			AND EV.EntityVersionName NOT IN 
 			(
 				'Procedure, Procedure'
 			)
+			AND EV.EntityVersionName NOT LIKE 'Audit%'
+			FOR XML PATH('')	
+		)
+		, 1, 1, '')						
 	)
-	SELECT TipoDado + ':' +  
-	(
-		REPLACE(SUBSTRING
-			(
-				(
-					SELECT EV.EntityVersionName + ',' AS [data()] FROM <BANCO_DADOS>.dbo.EntityVersion EV
-					INNER JOIN <BANCO_DADOS>.dbo.EntityType ET ON (ET.EntityTypeId = EV.EntityTypeId)
-					INNER JOIN <BANCO_DADOS>.dbo.Entity E ON (E.EntityTypeId = EV.EntityTypeId AND E.EntityId = EV.EntityId AND E.EntityLastVersionId = EV.EntityVersionId)
-					WHERE SUBSTRING(EntityVersionName, 1, 3) NOT IN ('Audit')
-					AND EntityVersionTimestamp BETWEEN CONVERT(DATETIME, CONVERT(VARCHAR(10), GETDATE() - <QUANTIDADE_DIAS_BACKUP>, 103) + ' 00:00:00') AND CONVERT(DATETIME, (CONVERT(VARCHAR(10), GETDATE(), 103) + ' 23:59:59'))
-					AND ET.EntityTypeName = TipoDado
-					AND EV.EntityVersionName NOT IN 
-					(
-						'Procedure, Procedure'
-					)
-					AND EV.EntityVersionName NOT LIKE 'Audit%'
-					FOR XML PATH('')						
-				),
-				1, 
-				LEN
-				(
-					(
-						SELECT EV.EntityVersionName + ',' AS [data()] FROM <BANCO_DADOS>.dbo.EntityVersion EV
-						INNER JOIN <BANCO_DADOS>.dbo.EntityType ET ON (ET.EntityTypeId = EV.EntityTypeId)
-						INNER JOIN <BANCO_DADOS>.dbo.Entity E ON (E.EntityTypeId = EV.EntityTypeId AND E.EntityId = EV.EntityId AND E.EntityLastVersionId = EV.EntityVersionId)
-						WHERE SUBSTRING(EntityVersionName, 1, 3) NOT IN ('Audit')
-						AND EntityVersionTimestamp BETWEEN CONVERT(DATETIME, CONVERT(VARCHAR(10), GETDATE() - <QUANTIDADE_DIAS_BACKUP>, 103) + ' 00:00:00') AND CONVERT(DATETIME, (CONVERT(VARCHAR(10), GETDATE(), 103) + ' 23:59:59'))
-						AND ET.EntityTypeName = TipoDado
-						AND EV.EntityVersionName NOT IN 
-						(
-							'Procedure, Procedure'
-						)
-						AND EV.EntityVersionName NOT LIKE 'Audit%'
-						FOR XML PATH('')							
-					)
-				) - 1), ', ', ',')
-	) + ';' AS [data()]
 	FROM CTE_TIPOS_DADOS
 	FOR XML PATH('')
+), 1, 1, '')
